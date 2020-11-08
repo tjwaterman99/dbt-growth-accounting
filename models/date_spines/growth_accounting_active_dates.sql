99b34ae5e0d01a49ca50f4492a91acf4498946af
Tom <tjwaterman99@gmail.com>
2020-11-08 20:28:32 +0000

    Add date spine for all active dates

---
+++ models/date_spines/growth_accounting_active_dates.sql
@@ -0,0 +1,22 @@
+/*
+I'm not really sure how to make the argument passing here more sensible without
+researching how these macros actually work.
+*/
+
+{{
+    config(materialized='table')
+}}
+
+{% set end_date = dbt_utils.get_query_results_as_dict('select max(active_at)::varchar end_date from ' ~ ref('growth_accounting_daily_activity__stg')) %}
+{% set start_date = dbt_utils.get_query_results_as_dict('select min(active_at)::varchar start_date from ' ~ ref('growth_accounting_daily_activity__stg')) %}
+{% set end_date_str = "'" ~ end_date['end_date'][0] ~ "'" %}
+{% set start_date_str = "'" ~ start_date['start_date'][0] ~ "'" %}
+{% set start_date_arg = "to_date(" ~ start_date_str ~ ", 'yyyy-mm-dd')" %}
+
+{{
+    dbt_utils.date_spine(
+        datepart='day',
+        start_date=start_date_arg,
+        end_date=end_date_str
+    )
+}}
